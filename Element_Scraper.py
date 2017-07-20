eda6f4191ed9d6b6422cd713b732626947537481
Chris Arroyo <carroyo@saltstack.com>
2017-07-19 22:23:42 -0600

    Adding Element Scraper python script

---
+++ Element_Scraper.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+
+output_id = "id = "
+
+user_url = raw_input("URL: " + " ")
+driver = webdriver.PhantomJS()
+driver.get(user_url)
+
+id_elements = driver.find_elements_by_xpath("//*[@id]")
+
+for eachElement in id_elements:
+    individual_ids = eachElement.get_attribute("id")
+    print(individual_ids)
+
+
