7e2ff371d6b2f725d3b0b5b2ab3cb5cfd9f2e129
Chris Arroyo <carroyo@saltstack.com>
2017-07-20 19:29:14 -0600

    Added option for element type

--- Element_Scraper.py
+++ Element_Scraper.py
@@ -1,16 +1,36 @@
 from selenium import webdriver
+from help_text import *
 
 
 output_id = "id = "
+output_name = "name = "
 
+print(url_help_text)
 user_url = raw_input("URL: " + " ")
+print(element_type_help_text)
+user_element_type = raw_input("Element Type: ")
+
+user_url = user_url.lower()
+user_element_type = user_element_type.lower()
+
+
 driver = webdriver.PhantomJS()
 driver.get(user_url)
 
 id_elements = driver.find_elements_by_xpath("//*[@id]")
+name_elements = driver.find_elements_by_xpath("//*[@name]")
 
-for eachElement in id_elements:
-    individual_ids = eachElement.get_attribute("id")
-    print(output_id + individual_ids)
+if user_element_type == "id":
+
+    for eachElement in id_elements:
+        individual_ids = eachElement.get_attribute("id")
+        print(output_id + individual_ids)
 
 
+elif user_element_type == "name":
+    for eachElement in name_elements:
+        individual_ids = eachElement.get_attribute("name")
+        print(output_name + individual_ids)
+
+else:
+    print("Invalid element type...")
