169ca11fc5eac4c71ae978ef5b581d7e630452b0
Chris Arroyo <arroyoc91@gmail.com>
2017-10-03 09:19:09 -0600

    Added comments. Added new choice 'all'

--- Element_Scraper.py
+++ Element_Scraper.py
@@ -14,20 +14,31 @@ user_url = user_url.lower()
 user_element_type = user_element_type.lower()
 
 
+# Browser of choice.. Use whichever webdriver you have available. Example: "webdriver.PhantomJS()", "webdriver.Chrome()"
 driver = webdriver.PhantomJS()
 driver.get(user_url)
 
+# Using selenium to find elements by xpath.
 id_elements = driver.find_elements_by_xpath("//*[@id]")
 name_elements = driver.find_elements_by_xpath("//*[@name]")
 
+# The following choice will print all id elements
 if user_element_type == "id":
-
     for eachElement in id_elements:
         individual_ids = eachElement.get_attribute("id")
         print(output_id + individual_ids)
 
-
+# The following choice will print all name elements
 elif user_element_type == "name":
+    for eachElement in name_elements:
+        individual_ids = eachElement.get_attribute("name")
+        print(output_name + individual_ids)
+
+# The following choice will print all of the options
+elif user_element_type == "all":
+    for eachElement in id_elements:
+        individual_ids = eachElement.get_attribute("id")
+        print(output_id + individual_ids)
     for eachElement in name_elements:
         individual_ids = eachElement.get_attribute("name")
         print(output_name + individual_ids)
--- help_text.py
+++ help_text.py
@@ -1,4 +1,4 @@
-element_types = ['id', 'name']
+element_types = ['id', 'name', 'all']
 
 url_help_text = "Please enter a valid URL.. ('http://google.com')"
 element_type_help_text = "Enter one of the following options: [%s]" % ', '.join(map(str, element_types))
